4b503283f2701f898fc67597f37de29f36514d42
Nadezhda Solodova <solodova.nadezhda.a@yandex.ru>
2021-04-20 18:27:57 +0300

    Added test

---
+++ nm_interpolate/test.c
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <vector>
+#include "interpolation.h"
+#include "l_exception.h"
+#include "point.h"
+
+static void test(vector<Point> &vec, int n, Real x, Real expected_result, int expected_error, char *message) {
+    int error = NA_OK;
+    Interpolation *points;
+    try {
+    	points = new Interpolation(vec, n, x, EPS);
+    }	catch (L_exception &err) {
+    	error = err.code();
+    }
+    Real result;
+    if(error = NA_OK) {
+    	try {
+    	result = points -> inter_lagrange(Real x);
+    	}	catch (L_exception &err) {
+    	error = err.code();
+    	}
+    }
+    
+    printf("Interpolated value = %lf\n", result);
+    if(expected_error != error) {
+    	printf("******* Test FAILED! WRONG ERROR_CODE******* %s\nRight ERROR = %d, Your error = %d\n", message, expected_error, error);
+        return;
+    }
+    if(expected_error == error && error != NA_OK) {
+    	printf("Test OK! Right error!\n");
+        return;
+    }
+    if(compareReal(result, expected_result, EPS)) {
+        printf("Test OK!\n");
+        return;
+    }
+    else {
+        printf("******* Test FAILED! WRONG ANSWER******* %s\nRight answer = %lf, Your answer = %lf\n", message, expected_result, result);
+        return;
+    }
+}
+
+int main( {
+	vector<Point> points1(3);
+	points[0] ={0, -1};
+	points[1] ={1, 1};
+	points[2] ={4, 1};
+    test(points1, 3, 2, 2, NA_OK, "Test 1");
+    return 0; 
+}
